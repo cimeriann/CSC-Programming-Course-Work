55f29975df0d91fd65f8245930b087ec1ab0a262
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-02-17 00:17:32 +0100

    upload files for task 2

--- Mortgage.c
+++ Mortgage.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * 
- * @return int 
+ * @return (0) - Success 
  */
 
 //function main begins program execution
@@ -16,20 +16,24 @@ int main( void )
     int totalAmount; // variable to hold the value of total amount
     int monthlyPayable; // variable to hold the value of amount payable per month after interest rate has been calculated
 
-    //prompt user for account number
-        printf("Enter account number (-1 to end) : ");
-        scanf("%d", &accountNumber);// read value entered and store in variable
 
     // keeps running until -1 is used to end the loop
     while (accountNumber != -1)
         {
+        //prompt user for account number
+        printf("Enter account number (-1 to end) : ");
+        scanf("%d", &accountNumber);// read value entered and store in variable
+
+        // break out of loop if user enters -1
+        if (accountNumber == -1) break;
+        
         //prompt the user for the mortgage amount
         printf("Enter mortgage amount (in dollars): ");
-        scanf("%f", &mortgageAmount);// read value entered and store in variable
+        scanf("%d", &mortgageAmount);// read value entered and store in variable
 
         //prompt the user for the mortgage term
         printf("Enter mortgage term (in years): ");
-        scanf("%f", &mortgageTerm); // read value entered and store in variable
+        scanf("%d", &mortgageTerm); // read value entered and store in variable
 
         //prompt the user for the interest rate
         printf("Enter interest rate (as a decimal): ");
