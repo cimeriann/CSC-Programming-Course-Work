ae0ad6facea6b9dedcb4cbd3526469fff8edee16
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-03-08 10:04:08 +0100

    solved coding tasks in chapter 4

---
+++ solutions/chapter4/salesCalculator.c
@@ -0,0 +1,12 @@
+//Sales Calculator
+#include <stdio.h>
+int main (void){
+    unsigned int productNumber;
+    unsigned int quantitySold;
+    scanf(" %u %u", &productNumber, &quantitySold);
+    while (/* condition */)
+    {
+        /* code */
+    }
+    
+}
