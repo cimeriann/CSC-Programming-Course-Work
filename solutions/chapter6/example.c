6009775b9d9f91dd48614cb1ce16498c88270aa4
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-06-12 08:42:19 +0100

    chapter6

---
+++ solutions/chapter6/example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void){
+    int n = 0, m = 0;
+    if (n > 0){
+        if (m > 0){
+            printf("True");
+        }
+    }
+    else{
+        printf("False");
+    }
+}
