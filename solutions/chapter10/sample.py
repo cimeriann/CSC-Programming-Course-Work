7c3a8fc4ed8e46128da86570a9b518c7a1a30319
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-06-15 21:11:41 +0100

    updated chapter10

---
+++ solutions/chapter10/sample.py
@@ -0,0 +1,48 @@
+# myList = [1,2,3,4]
+# if (2 == myList):
+#     print("found ya!")
+# else:
+#     print("nothing found")
+# def break_in_loop():
+
+#     for i in range(0, 5):
+#         print(i)
+        
+#     else: 
+#         print("Finally!")
+
+# break_in_loop()
+
+# def break_example():
+#     y = 5
+#     for i in range(0, y+1):
+#         if (i == y):
+#             print("Thank you!")
+#             break
+#         else:
+#             print(i)
+
+# break_example()
+# i = 0
+# while i < 3:
+#     print(i)
+#     i += 1
+#     # if i == 3:
+#     #     break
+# else: 
+#     print(0)
+# print(int(1011))
+# for i in range(10):
+
+#     if i == 5:
+#         break
+#     else:  print(i)
+# else: 
+#     print("Here")
+
+# x = float(2)
+# print(type(x))
+a = 2.13
+b = 3.7777
+c = -3.12
+print(int(a))
