0742145e9fc0f11b898774ae21407918d13a179e
Olayinka <adebisiolayinka30@gmail.com>
2023-09-01 17:14:16 +0100

    used scanner for fetching user input

---
+++ JavaForCsc/src/userInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        
+        int age = scanner.nextInt();
+        System.out.println("Hello, " +  name);
+        System.out.println("You are "+ age + " years old");
+        scanner.close();
+    }
+}
