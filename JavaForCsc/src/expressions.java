a80184ce882e1f3cfabcef902a28c63f7f62cc6f
Olayinka <adebisiolayinka30@gmail.com>
2023-09-01 19:57:56 +0100

    intro to GUI's

---
+++ JavaForCsc/src/expressions.java
@@ -0,0 +1,9 @@
+public class expressions {
+    public static void main(String[] args) throws Exception {
+        // typecasting
+        double myNum = 20;
+
+        myNum = (double) myNum / 3;
+        System.out.println(myNum);
+    }
+}
