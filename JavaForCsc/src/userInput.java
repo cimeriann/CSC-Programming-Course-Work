722f9139a2b82757ace3b4b75a8ff1ee9d5cac55
Olayinka <adebisiolayinka30@gmail.com>
2023-09-01 18:20:21 +0100

    fixed scanner issue

--- JavaForCsc/src/userInput.java
+++ JavaForCsc/src/userInput.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
-public class userInput {
+public class userInput{
     public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("What is your name? ");
-        String name = scanner.nextLine();
-        System.out.println("How old are you? ");
+        String name;
+        int age;
         
-        int age = scanner.nextInt();
-        System.out.println("Hello, " +  name);
-        System.out.println("You are "+ age + " years old");
+        System.out.println("What is your name? ");
+        name = scanner.nextLine();
+        System.out.println("How old might you be? ");
+        age = scanner.nextInt();
+        // calling nextline after the next int should clear the scanner
+        scanner.nextLine();
+        // bug arises, as scanner doesnt wait to accept input for food
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello, " + name);
+        System.out.println("You are " + age + " years old.");
+        System.out.println("You like " + food);
+
         scanner.close();
+
     }
 }
