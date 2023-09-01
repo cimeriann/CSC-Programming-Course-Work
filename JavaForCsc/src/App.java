6b4720ac70456833518362d0335d40ebfa62e947
Olayinka <adebisiolayinka30@gmail.com>
2023-09-01 10:04:26 +0100

    init java project

---
+++ JavaForCsc/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\"Hello, World! in quotes\"");
+        // shortcut to generate the standard print line statement
+        // sysout
+        // spaces dont really matter too much depending on where they are
+    }
+}
