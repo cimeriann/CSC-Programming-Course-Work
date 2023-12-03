f2b8761ac07c4f4ad7265068c15a97b71454d002
Adebisi Olayinka <adebisiolayinka30@gmail.com>
2023-12-03 16:07:38 +0100

    add other linear search methods

--- algorithms/src/LinearSearchAlgorithm.java
+++ algorithms/src/LinearSearchAlgorithm.java
@@ -55,4 +55,18 @@ public class LinearSearchAlgorithm {
         }
         return false;
     }
+    static int linearSearchRange(int[] arr, int target, int start, int end){
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int index = start; index <= end; index++) {
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+
+        }
+        return -1;
+
+    }
 }
